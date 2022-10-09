4c8e6814e3f29e46aef87aa57402f37fcd0d7be7
mi nombre <naranjosa2004@gmail.com>
2022-10-08 19:02:10 -0500

    Variables

---
+++ Variables.java
@@ -0,0 +1,44 @@
+public class Variables {
+    public static void main(String[]args) {
+        
+        //Este es un Comentario de Linea Simple
+        /*
+         * 
+         * Este es un Comentario Multilinea
+         */
+
+/*
+ * Para declarar una variable, primero está:
+ * El Tipo de dato, el nombre, = , lo que quieres que esté en ese tipo de dato,;
+ */
+
+
+
+         //Esta es una variable de Texto
+        String texto = "Hola Mundo Me Llamo Santiago";
+    //Esta es una variable de Caracteres
+        char letra = 'A';
+        //Esta es una variable de tipo Entero
+        int a = 5;
+        //Esta es una varible de tipo Decimal
+        double b = 5.5;
+        //Esta es una variable de tipo Flotante
+        float d = 2.9f;
+        //Esta es una variable de tipo Short
+        short e = 23;
+        //Esta es una variable de tipo Long
+        long f = 102131823;
+        //Esta es una variable de tipo Boolean
+        boolean g = true;
+        
+        //Impresión de la variable texto
+        System.out.println(texto);
+        //Imprimiendo: El numero es 5
+        System.out.println("El numero es: "+a);
+        System.out.println(b);
+
+   
+
+        
+    }
+}
